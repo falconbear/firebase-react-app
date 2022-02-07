c79c93491d6b69b9b31a53e319dc5651e75fc91b
falconbear <96473284+falconbear@users.noreply.github.com>
2022-02-07 22:08:11 +0900

    デプロイ

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react"
+import { memo } from 'react'
+import Message from './Message'
+import useApp from '../hooks/useApp'
 
-const App = () => {
-  return <div>hello</div>
-}
+const App = memo(() => {
+  const { setNameFunc, setTextFunc, pushMessageToFirebase, messages, data } =
+    useApp()
+
+  return (
+    <>
+      {messages.map((message) => (
+        <Message key={message.key} message={message} />
+      ))}
+      <input type="text" value={data.name} onChange={(e) => setNameFunc(e)} />
+      <input type="text" value={data.text} onChange={(e) => setTextFunc(e)} />
+      <button onClick={() => pushMessageToFirebase()}>push</button>
+    </>
+  )
+})
 
 export default App
