2f786e770e96c68f421de27d59302d6db72bd890
falconbear <96473284+falconbear@users.noreply.github.com>
2022-02-22 16:49:10 +0900

    componentsAの仮実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Login from './components/Login'
+import Root from './components/Root'
+import ComponentA from './components/ComponentA'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Root />} />
+          <Route path="/component-a" element={<ComponentA />}/>
+        </Route>
       </Routes>
     </div>
   )
