f030adb2c881395571b3aac25c82e7f61458b6a3
falconbear <96473284+falconbear@users.noreply.github.com>
2022-02-22 16:41:49 +0900

    Login追加

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { StateProvider } from './context/StateProvider'
 import Layout from './components/Layout'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <div>
       <StateProvider>
+        <Login />
         <Layout />
       </StateProvider>
     </div>
