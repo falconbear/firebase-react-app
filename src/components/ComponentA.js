e22c0b6b59d53de4329902a7275e73d4bc3ff6ad
falconbear <96473284+falconbear@users.noreply.github.com>
2022-02-22 17:12:20 +0900

    chatページ追加

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,12 +1,48 @@
 import { useNavigate } from 'react-router-dom'
 import { useStateContext } from '../context/StateProvider'
+import { useState, useEffect } from "react"
+import { messagesRef, pushMessage } from "../firebase"
 
 const ComponentA = () => {
+  const [text, setText] = useState("メッセージを入力")
+  const [messages, setMessages] = useState([])
   const navigate = useNavigate()
   const { isOn, setIsOn } = useStateContext()
 
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(10)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val()
+        if (messages === null) return
+        const entries = Object.entries(messages)
+        const newMessages = entries.map((data) => {
+          const [key, message] = data
+          return { key, ...message }
+        })
+        setMessages(newMessages)
+      })
+  }, [])
+
   return (
     <div className="flex justify-center items-center flex-col">
+      {messages.map((message) => (
+        <div key={message.key}>
+          {message.text}
+        </div>
+      ))}
+      
+      <div class="form">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText((text) => (text = e.target.value))}
+        />
+        <button onClick={() => pushMessage({ text: text })}>
+          push
+        </button>
+      </div>
       <p className="font-bold my-1">ComponentA</p>
       <p onClick={() => navigate('/')}>Go Root</p>
       on? off?:{isOn ? 'on' : 'off'}
