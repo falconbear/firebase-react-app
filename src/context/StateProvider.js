f20d1cc73e6eb4527b34442ae17f35e7d139269d
falconbear <96473284+falconbear@users.noreply.github.com>
2022-02-22 16:55:42 +0900

    コンポーネントの実装

--- src/context/StateProvider.js
+++ src/context/StateProvider.js
@@ -5,9 +5,12 @@ const StateContext = createContext({})
 export const StateProvider = ({ children }) => {
   const serviceName = 'Super Web Site'
   const [isLogin, setIsLogin] = useState(false)
+  const [isOn, setIsOn] = useState(false)
 
   return (
-    <StateContext.Provider value={{ serviceName, isLogin, setIsLogin }}>
+    <StateContext.Provider
+      value={{ serviceName, isLogin, setIsLogin, isOn, setIsOn }}
+    >
       {children}
     </StateContext.Provider>
   )
